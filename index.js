0de5833cc4b175099f644ec6dcba2266965c3f08
Fusko1 <madeinchina15@yandex.ru>
2025-05-16 11:23:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-const express = require("express");
-const fetch = require("node-fetch");
-const app = express();
+const fetch = require('node-fetch');
 
-const API_KEY = process.env.API_KEY;
-const NICKNAME = process.env.NICKNAME;
+module.exports = async (req, res) => {
+  const API_KEY = process.env.API_KEY;
+  const NICKNAME = process.env.NICKNAME;
 
-app.get("/", async (req, res) => {
   try {
-    const url = `https://open.faceit.com/data/v4/players?nickname=${NICKNAME}`;
-    const result = await fetch(url, {
+    const response = await fetch(`https://open.faceit.com/data/v4/players?nickname=${NICKNAME}`, {
       headers: {
         Authorization: `Bearer ${API_KEY}`,
       },
     });
-    const data = await result.json();
-    const elo = data.games.cs2.faceit_elo;
-    res.setHeader("Content-Type", "text/plain");
-    res.send(`${elo}`);
-  } catch (e) {
-    res.status(500).send("Ошибка");
-  }
-});
 
-module.exports = app;
+    const data = await response.json();
+    const elo = data.games.cs2.faceit_elo;
+
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(200).send(`${elo}`);
+  } catch (error) {
+    res.status(500).send('Ошибка получения ELO');
+  }
+};
