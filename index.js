1ebf7276cf6a1ed13ca057adc35cccbe250722d9
Fusko1 <madeinchina15@yandex.ru>
2025-05-16 11:32:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const fetch = require('node-fetch');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 module.exports = async (req, res) => {
   const API_KEY = process.env.API_KEY;
   const NICKNAME = process.env.NICKNAME;
+
+  if (!API_KEY || !NICKNAME) {
+    res.status(500).send("API_KEY или NICKNAME не заданы в переменных окружения");
+    return;
+  }
 
   try {
     const response = await fetch(`https://open.faceit.com/data/v4/players?nickname=${NICKNAME}`, {
@@ -12,11 +17,16 @@ module.exports = async (req, res) => {
     });
 
     const data = await response.json();
-    const elo = data.games.cs2.faceit_elo;
 
-    res.setHeader('Content-Type', 'text/plain');
+    if (!data.games || !data.games.cs2 || !data.games.cs2.faceit_elo) {
+      res.status(500).send("Ошибка получения ELO");
+      return;
+    }
+
+    const elo = data.games.cs2.faceit_elo;
+    res.setHeader("Content-Type", "text/plain");
     res.status(200).send(`${elo}`);
-  } catch (error) {
-    res.status(500).send('Ошибка получения ELO');
+  } catch (err) {
+    res.status(500).send("Ошибка сервера: " + err.message);
   }
 };
